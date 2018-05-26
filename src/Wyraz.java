d77c392abe3577daa6a563aff66406b70aca3660
Marek Procek <marekprocek@gmail.com>
2018-05-26 13:06:54 +0200

    zadanie-java-46

---
+++ src/Wyraz.java
@@ -0,0 +1,7 @@
+public class Wyraz {
+    public static void main(String[] args) {
+        System.out.print((char)75);
+        System.out.print((char)79);
+        System.out.print((char)84);
+    }
+}
